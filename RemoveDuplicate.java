6f68228326c75ebdc35dac612d4ece32f25d055e
pallavi-gatade <pallavi.gatade24@gmail.com>
2024-11-18 16:28:35 +0530

    Add files via upload

---
+++ RemoveDuplicate.java
@@ -0,0 +1,35 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class RemoveDuplicate {
+
+	public static void removeDuplicateUsingSorting(int arr[]) {
+
+		Arrays.sort(arr);
+
+		int len = arr.length;
+		int j = 0;
+
+		for (int i = 0; i < len - 1; i++) {
+
+			if (arr[i] != arr[i + 1]) {
+				arr[j++] = arr[i];
+			}
+		}
+
+		arr[j++] = arr[len - 1];
+
+		for (int k = 0; k < j; k++) {
+			System.out.print(arr[k] + " ");
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int arr[] = { 5, 1, 2, 6, 4, 4, 5, 6, 8, 7 };
+		removeDuplicateUsingSorting(arr);
+
+	}
+}
